3e001c41bedd749237e8a37a5c655afa7a1b5765
David-Yuba <dado2012yuba@gmail.com>
2025-08-25 18:51:17 +0200

    Added cursor:pointer and null checks into onClick function

--- scripts/faq.js
+++ scripts/faq.js
@@ -2,6 +2,8 @@ let acord = document.getElementsByClassName("acordion")[0];
 
 acord.addEventListener("click",onClick);
 
+
 function onClick(event){
-    event.target.nextElementSibling.classList.toggle("active");
+    let acordionParagraph = event.target.nextElementSibling;
+    if(acordionParagraph) acordionParagraph.classList.toggle("active");
 }
